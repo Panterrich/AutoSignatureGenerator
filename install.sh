a22a559127bcd523fa62bfb9935e4bbc7d8aefe2
panterrich <durnov.an@phystech.edu>
2024-01-24 18:14:43 +0300

    feature: add bibliograhy

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ fi
 
 apt install -y texlive-latex-base
 apt install -y texlive-fonts-recommended
+apt install -y texlive-bibtex-extra
 apt install -y texlive-fonts-extra
 apt install -y texlive-latex-extra
 apt install -y texlive-lang-cyrillic
